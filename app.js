be8aed1d8d224d17356eae5e611e7e003355d985
Smgy94 <Shanemcgeown@gmail.com>
2023-07-01 00:22:23 +0100

    commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
 const bars = document.querySelectorAll(".bar");
 const navWrapper = document.getElementById("nav-wrapper");
 const navLinks = document.getElementById("nav-links");
+const body = document.querySelector("body");
 
 mobileMenuTrigger.addEventListener("click", function () {
   mobileMenuTrigger.classList.toggle("active");
   navWrapper.classList.toggle("active");
   navLinks.classList.toggle("active");
   bars.forEach((bar) => bar.classList.toggle("active"));
+  body.classList.toggle("menu-open"); // Add or remove "menu-open" class to the body
 });
